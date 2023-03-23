5f96345857ece4bdd24510556d2009f029c1e152
HAndradeSJ <henriqueandradesj@outlook.com>
2023-03-23 13:58:46 -0300

    version.0.7

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 // Importação
 const express = require('express');
+const exphbs = require('express-handlebars')
 
 
 // Definindo variaveis pardrão
 const app = express();
 
 // Definido minha view egine
+// define qual o template a ser utilizado
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+// define a extensão e a instância do handlebars com o modelo que será interpretado o código
+app.engine( 'hbs', exphbs.engine( {
+    extname: '.hbs',
+    defaultView: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/'
+}));
 
 
 
--- src/controllers/form.js
+++ src/controllers/form.js
@@ -1,12 +1,11 @@
-const path = require('path');
-const location = path.join(__dirname + '../views');
+
 // Defindo a class do meus controllers
 class Home {
     
     // controller para renderizar a home
     renderhome(params) {
         return (req,res)=> {
-            res.sendfile(location +"home.html");
+            res.render('home');
         }
    } 
 
