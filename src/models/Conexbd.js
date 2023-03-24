741319d3759cb84be58bc70e1709ab54e2aa8978
ejsjose <ejsjose@kyndryl.com>
2023-03-24 11:06:27 -0300

    troca do mysql to sqlite

--- src/models/Conexbd.js
+++ src/models/Conexbd.js
@@ -2,15 +2,26 @@
 const Sequelize = require('sequelize');
 
 // fazendo a conexção do banco de dados
-const sequelize = new Sequelize('testehenrique','root','1234',{
-    host: 'localhost',
-    dialect:'mysql'
-    
-})
+var sequelize = new Sequelize(
+    "testehenrique",
+    "root",
+    "1234",
+    {
+        host: "0.0.0.0",
+        dialect: "sqlite",
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+        storage: "src/models/testehenrique.sqlite"
+    }
+);
+  
 
 // promise para validar a conexação
-sequelize.authenticate().then(result => res.status(200).send(result))
-.catch(err => res.status(500));
+// sequelize.authenticate().then(result => res.status(200).send(result))
+// .catch(err => res.status(500));
 
 
 // exportação da constante do banco de dados
