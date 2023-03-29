f9815032ab6db8393a6d50aafb837d3687d8bc5e
HAndradeSJ <henriqueandradesj@outlook.com>
2023-03-29 14:17:25 -0300

    version ajuste imgs

--- src/services/criardocuemento.services.js
+++ src/services/criardocuemento.services.js
@@ -1,47 +1,14 @@
 
 //  Importação
 const http = require('http');
-const htmlDocx= require('html-docx-js');
-const fs = require('fs');
+
     class geratordocumentos {
       criarmodelo1(req,res){
-        var  pretitulo = req.body.titulo
-        var  namearquivo = req.body.nomearquivo
-        var  pretextarea = req.body.desc
-        var  itemlista = req.body.itemlista
+     
 
-        var semilista = '<ul>'
-        var contador = 1
-        while(contador<=itemlista){
-         semilista= semilista + `<li>item</li>`
-         contador = contador + 1
-        }
-        lista= semilista + '</ul>'
-    
-        var titulo = `<h1>${pretitulo}</h1>`
-        var desc = `<p>${pretextarea}</p>`
-        var Abertura = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Gerator Word</title></head><body>"
-        var fechamento = "</body></html>"
-        var documento = Abertura +titulo+lista+desc+fechamento;
-
-        const docx = htmlDocx.asBlob(documento);
-
-        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        res.setHeader('Content-Disposition', `attachment; filename=${ sluggify(namearquivo) }.docx`);
-        res.setHeader('Content-Length', docx.length);
-        (url,docx,req)=>{
-                  const file = fs.createWriteStream(docx);
-                  http.get(url, function(response) {
-                    response.pipe(file);
-                    file.on('finish', function() {
-                      file.close(); 
-                    })
-                  })
-        }
-
-      }
-    
- }
+        
+  }
+}
 
   
         
