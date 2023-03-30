db40a77f6f2f7d1626553869ef9effc95a402e60
HAndradeSJ <henriqueandradesj@outlook.com>
2023-03-30 14:26:55 -0300

    version services

--- src/services/criardocuemento.services.js
+++ src/services/criardocuemento.services.js
@@ -1,62 +1,55 @@
 
 //  Importação
 const https = require('http');
-const htmlDocx= require('html-docx-js');
+const officegen = require('officegen');
 const fs = require('fs');
+
     class geratordocumentos {
       criarmodelo1(req,res){
         // pegando os dados do corpo 
         var  pretitulo = req.body.titulo
         var  namearquivo = req.body.nomearquivo
         var  pretextarea = req.body.desc
-        var  itemlista = req.body.itemlista
+        var  itemlista = req.body.itemlista;
 
-        // função para gerar a lista 
-        var semilista = '<ul>'
+        // Genrando a lista
+        var semilista = '/n'
         var contador = 1
-        while(contador<=itemlista){
-         semilista= semilista + `<li>item</li>`
-         contador = contador + 1
+        while (contador <=itemlista){
+          semilista = semilista + 'item'+'/n'
+          contador = contador + 1
         }
-        var lista = semilista + '</ul>'
+        var plista = semilista + '/n'
+        // Criando documetno do word em branco
+        let  documento  = officegen('documento');
 
-        // montando as partes do arquivo html
-        var titulo = `<h1>${pretitulo}</h1>`
-        var desc = `<p>${pretextarea}</p>`
-        var Abertura = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Gerator Word</title></head><body>"
-        var fechamento = "</body></html>"
-        var documento = Abertura +titulo+lista+desc+fechamento;
+        // Colocando os dados dentro do documento em branco
+        var titulo = documento.createP({align:'center'});
+          // estilizando a o elemento
+          titulo.addText(pretitulo,{
+            border:'solid',
+            borderSize: 12,
+            borderColor:'88CFF'
+          });
 
-        // Convetendo o arquivo html in docxs
-        const docx = htmlDocx.asBlob(documento);
+        var lista = documento.createP({align:'left'});
+        lista.addText(plista,{bold:true,underline:true});
 
-        // Fazendo donwload do documento
-        var url = 'https://application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(docx);
-        namearquivo = namearquivo?namearquivo +'.doc': 'document.doc';
+        var desc = documento.createP({align:'justify'});
+        desc.addText(pretextarea,{bold:true,underline:true});
 
-        https.get(url,(res)=>{
-          const arquivo = fs.createWriteStream(docx)
-          res.pipe(arquivo);
+        // Nomeando o arquvio 
+        res.writeHead(200,{
+          "content-type": "application/vnd.openxmlformats-officedocument.documentml.document",
+          "content-disposition": "attachment; filename = " + namearquivo
         })
-        arquivo.on('error', (err)=>{
-          console.error(err);
+        // fazendo download do docsx
+        documento.generate(res);
 
-        })
-
-
-        arquivo.on("criado",()=>{
-          arquivo.close();
-          console.log("Feito")
-        })
-
-    
-
-
-        
-        
 
       }
-    
+  
+
  }
 
 module.exports = geratordocumentos;
